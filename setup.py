de54ee8c6143a0a6dbb0f5d9a08ec8e7d3d665ec
Denis Zalevskiy <denis@visfun.org>
2018-03-05 08:18:11 +0200

    Update setup metadata
    Signed-off-by: Denis Zalevskiy <denis@visfun.org>

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup, find_packages
+
+long_description = '''
+'''
+
 setup(
     name="cor",
-    version="0.1",
-    packages=find_packages(),
+    version="0.2",
+    description="Algebraic Data Types et al.",
+    long_description=long_description,
+    url="https://github.com/deztructor/pycor",
+    author="Denis Zalevskiy",
+    author_email="denis@visfun.org",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords="adt contract types development",
+    packages=find_packages(exclude=['tests', 'examples']),
     test_suite='tests',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
